499713eb3d9072d716d08a978a735408f3854657
Achuan-2 <achuan-2@outlook.com>
2021-10-15 21:35:09 +0800

    :memo: export temporary variable

--- main.sh
+++ main.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-export version="v0.8.9"
 export THEME="Tsundoku"
+#--！-----每次更新记得修改更新信息-----------------#
+export version="v0.9.4"
+export update_content="change blockquote style"
+#--！----------------------#
 bash ./1_repo_commit.sh
 bash ./2_get_sha1.sh
 bash ./3_siyuan_bazzar.sh
