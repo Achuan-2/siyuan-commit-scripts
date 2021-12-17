f5e08a1cdcf625748655dcb2e35583befc1822a7
Achuan-2 <achuan-2@outlook.com>
2021-12-17 17:49:48 +0800

    :memo:

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 export THEME="Tsundoku"
 #--！-----每次更新记得修改更新信息-----------------#
-export version="v0.9.4"
-export update_content="change blockquote style"
+export version="v1.0.3"
+export update_content="✨分割线样式完善"
 #--！----------------------#
 bash ./1_repo_commit.sh
-bash ./2_get_sha1.sh
+# bash ./2_get_sha1.sh
 bash ./3_siyuan_bazzar.sh
