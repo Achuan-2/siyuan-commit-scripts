c46fe6bc56f5d9a8f4c9a0bda93877296e6c2263
Achuan-2 <achuan-2@outlook.com>
2022-02-04 19:03:04 +0800

    🎨 适配目前思源笔记集市不允许作者编辑json文件

--- main.sh
+++ main.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 export THEME="Tsundoku"
 #--！-----每次更新记得修改更新信息-----------------#
-export version="v1.0.3"
-export update_content="✨分割线样式完善"
+export message=":lipstick: 修改border color"
 #--！----------------------#
 bash ./1_repo_commit.sh
-# bash ./2_get_sha1.sh
-bash ./3_siyuan_bazzar.sh
+bash ./2_get_sha1.sh
+# bash ./3_siyuan_bazzar.sh
