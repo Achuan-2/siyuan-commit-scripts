a0e7e213805e3572d39ebb78f4c774bccb30b31f
Achuan-2 <achuan-2@outlook.com>
2022-03-09 15:04:25 +0800

    :memo:

--- main.sh
+++ main.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 export THEME="Tsundoku"
 #--！-----每次更新记得修改更新信息-----------------#
-export message=":lipstick: 修改border color"
+# :lipstick:取消页签动画
+# :bookmark: v1.1.2
+# :sparkles: 更新插件
+# :art: [theme.css] reorganize
+export message=":art: [theme.css] reorganize"
+
 #--！----------------------#
 bash ./1_repo_commit.sh
 bash ./2_get_sha1.sh
+
+
 # bash ./3_siyuan_bazzar.sh
---
+++ tag.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo -e "\e[1;31m\nRun tag.sh\n\e[0m" 
+VERSION="v1.1.2"
+cd ../
+
+HOME=`pwd`
+echo $message
+find ${HOME} -maxdepth 1 -type d -name "siyuan-themes*" | while read repo;
+    do  
+        echo -e "\e[1;31mTagging\e[0m" $repo
+        cd $repo
+        echo `pwd`
+        git tag $VERSION
+        git push origin --tags
+        echo -e "done\n"
+done
