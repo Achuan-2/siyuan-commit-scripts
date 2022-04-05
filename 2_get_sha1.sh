ab31c051c593a5e486e2557896d63628ed0f67a2
Achuan-2 <achuan-2@outlook.com>
2022-04-05 18:43:28 +0800

    ✨ optimize get_sha1

--- 2_get_sha1.sh
+++ 2_get_sha1.sh
@@ -10,8 +10,8 @@ THEME=`pwd`
 # generate json
 echo -e "\e[1;36m\n$THEME temp.json.json started generating\n\e[0m"
 echo "" > ${SCRIPTS}/temp.json
-echo -e "{ \n    \"repos\": [" >> ${SCRIPTS}/temp.json
-
+echo -e "{ \n\t\"repos\": [" >> ${SCRIPTS}/temp.json
+echo -e "\t\"start\"," >> ${SCRIPTS}/temp.json
 # find + absolute path -> return absolute path
 find ${THEME} -maxdepth 1 -type d -name "siyuan-themes*" | while read repo;
     do  
@@ -19,11 +19,11 @@ find ${THEME} -maxdepth 1 -type d -name "siyuan-themes*" | while read repo;
         # rexp: (?<=exp) match after the exp
         # grep : -P use regular expression，-o print the matched parts
         # git rev-parse HEAD -> get the latest SHA-1
-        git remote -v | tail -n 1 | grep -P "(?<=github.com\/)[^\.]+" -o | xargs -i echo "    \"{}@`git rev-parse HEAD`\","  
+        git remote -v | tail -n 1 | grep -P "(?<=github.com\/)[^\.]+" -o | xargs -i echo -e "\t\"{}@`git rev-parse HEAD`\","  
 
 done >> ${SCRIPTS}/temp.json 
-
-echo -e "    ] \n}" >> ${SCRIPTS}/temp.json
+echo -e "\t\"end\"" >> ${SCRIPTS}/temp.json
+echo -e "\t]\n}" >> ${SCRIPTS}/temp.json
 
 # info
 echo -e "\e[1;36m\n${SCRIPTS}/temp.json.json generated successfully\n\e[0m"
