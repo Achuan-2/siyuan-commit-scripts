4e9e5cf2ff5d845231db087728f533f97ef0e557
Achuan-2 <achuan-2@outlook.com>
2022-04-08 13:00:53 +0800

    :memo:

--- main.sh
+++ main.sh
@@ -2,30 +2,25 @@
 
 SCRIPT=`pwd`
 
-
 run(){
     ####主题日常 commit####
     # message=":bookmark: v1.2.4"
     # commit
 
-    ####上交到集市####
+    ####上交到集市,自行前往https://github.com/Achuan-2/bazaar pull request####
     VERSION="v1.2.4"
     release
 }
 release() {
     source ${SCRIPT}/3_tag.sh
     # 添加标签
-    # add_tag $1
+    add_tag $VERSION
     # 发布到集市
     cd  ${SCRIPT}
     bash ${SCRIPT}/4_bazaar.sh $VERSION
 
-    #自行前往https://github.com/Achuan-2/bazaar pull request
 }
 commit() {
-    #---------1.commit--------------#
     bash ${SCRIPT}/1_repo_commit.sh $message
-
-
 }
 run
